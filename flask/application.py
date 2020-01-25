50bb8d19d2d3a959a8fb1d0af2165a123c43f369
Kareem Dabbour <47406764+KareemDabbour@users.noreply.github.com>
2020-01-25 14:32:13 -0500

    Add files via upload

--- flask/application.py
+++ flask/application.py
@@ -2,5 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-	return ("test gang")
+def home():
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	app.run(debug = True)
