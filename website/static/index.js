d0c8abc347bc26052788e2df5838b33239644b31
hackerrank <me@hackerrank.com>
2020-01-25 23:47:04 -0500

    fixed colors

---
+++ website/static/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $("#happy").click(function(){
+        $("body").css({"background": "linear-gradient(120deg, #FF9A54 , #FFEA6B, #54FEFB, #E7B2FF, #89FFCB)",
+            "background-size": "1000% 1000%",
+                
+            "-webkit-animation": "AnimationName 10s ease infinite",
+            "animation": "AnimationName 10s ease infinite"})
+      });
+    }
+)
+$(document).ready(function () {
+    $("#sad").click(function(){
+        console.log("run");
+        $("body").css({"background": "linear-gradient(120deg, #494487, #3C3175, #522A66, #3B1840, #230B21)",
+            "background-size": "1000% 1000%",
+                
+            "-webkit-animation": "AnimationName 10s ease infinite",
+            "animation": "AnimationName 10s ease infinite"})
+      });
+    }
+)
+$(document).ready(function () {
+    $("#calm").click(function(){
+        $("body").css({"background": "linear-gradient(120deg, #280F36, #632B6C, #C76C98, #F09F9C, #FDC2A2)",
+            "background-size": "1000% 1000%",
+                
+            "-webkit-animation": "AnimationName 10s ease infinite",
+            "animation": "AnimationName 10s ease infinite"})
+      });
+    }
+)
+
--- website/templates/index.js
+++
@@ -1,4 +0,0 @@
-function notify(){
-    alert("clicked");
-}
-$("#audio1").on("click", notify);
